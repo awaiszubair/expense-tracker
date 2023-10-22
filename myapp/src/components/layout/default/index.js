b10318fe0615ec744b3045184058016fbf8fe8d1
Coder Dreamer <76837080+awaiszubair@users.noreply.github.com>
2023-10-22 17:15:24 +0500

    Delete myapp directory

--- myapp/src/components/layout/default/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const defaultlayout = (props) => {
-  return <div className="container">{props.children}</div>;
-};
-
-export default defaultlayout;
